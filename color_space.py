be7442a6778cc3883ab40e2e434a5ed2cdb11f01
unknown <barry26653@gmail.com>
2023-06-09 11:34:40 +0800

    modify gitignore and color_space

--- color_space.py
+++ color_space.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def rgd_2_ycbcr(mcu):
     
-    return np.random.randint(0, 256, (16, 16)), np.random.randint(0, 256, (8, 8)), np.random.randint(0, 256, (8, 8)) 
+    return np.random.randint(0, 256, (16, 16)), np.random.randint(0, 256, (8, 8)), np.random.randint(0, 256, (8, 8))
 
 def ycbcr2rgb(img):
 
