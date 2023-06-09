48c056a5227cd5aaf7f842aeb182b8087a61928d
ChingHuanWang <b08901033@ntu.edu.tw>
2023-06-09 11:14:56 +0800

    fix get dc component error, and split file into ac and dc table

--- ac_huffman_table.py
+++ ac_huffman_table.py
@@ -13,6 +13,24 @@ def get_chrom_cr_ac(ac):
     chrom_cr_ac = np.array([block[63*5:63*6] for block in ac])
     return chrom_cr_ac.flatten()
 
+def init_prob_and_category():
+    run = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+    size = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A"]
+    category = ["0/0"]
+    for r in run:
+        for s in size:
+            category.append(r+"/"+s)
+            
+    prob = np.zeros(len(category))
+    return prob, category
+
+def get_prob_and_category(component):
+    
+    prob, category = init_prob_and_category()
+    
+    
+    
+
 def lum_ac_huffman_table(ac):
     
     size_table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
